3a44a8dbe4166b89c4ed346223af666b235de5aa
Gleder <glederson_cf@yahoo.com.br>
2023-07-12 14:42:42 -0300

    Primeiro MCV Department

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            departments.Add(new Department { Id = 1, Name = "Eletronics" });
+            departments.Add(new Department { Id = 2, Name = "Fashion" });
+            departments.Add(new Department { Id = 3, Name = "Computers" });
+
+            return View(departments);
+        }
+    }
+}
